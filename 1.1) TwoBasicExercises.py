12eebb6c28333bb0568d5e8d47dbaee51679258e
Aylmz0 <142519027+Aylmz0@users.noreply.github.com>
2024-10-29 04:00:08 +0300

    Create 1.1) TwoBasicExercises.py

---
+++ 1.1) TwoBasicExercises.py
@@ -0,0 +1,13 @@
+#Dikdörtgen alan hesabı
+length = float(input("Enter the length: "))
+width = float(input("Enter the width: "))
+area = length * width
+
+print(f"Alan: {area} cm^2 ")
+
+#Alışveriş kartı programı
+item = input("Enter the item: ")
+price = float(input("Enter the price: "))
+quantity = int(input("Enter the quantity: "))
+total_price = price * quantity
+print(f"Total price is ${total_price}")
